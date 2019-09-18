38f4914c6c8b5466a5c3f210903ab95e5cee8a6e
s1293 <sunr@dtdream.com>
2019-09-18 10:01:23 +0800

    messagepack 0917

--- src/main/java/com/sr/nio/netty/date0917/User.java
+++ src/main/java/com/sr/nio/netty/date0917/User.java
@@ -2,6 +2,7 @@ package com.sr.nio.netty.date0917;
 
 import org.msgpack.annotation.Message;
 
+// 这个注解一定要加
 @Message
 public class User {
     private String name;
@@ -22,7 +23,7 @@ public class User {
     public void setAge(int age) {
         this.age = age;
     }
-
+    // 要有无参构造器
     public User() {
     }
 
